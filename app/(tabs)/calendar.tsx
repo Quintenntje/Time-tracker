a97e935ac86a3fefe1638ea338c0c81a91823051
Quinten <quintenclaes7@gmail.com>
2025-06-30 01:07:33 +0200

    Refactor Calendar component to improve state management and enhance day selection functionality

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { Text, useColorScheme, View } from "react-native";
+import CustomBottomSheet from "../../components/BottomSheet";
 import CalendarDay from "../../components/CalendarDay";
 import PageLayout from "../../components/PageLayout";
 
-export default function Index() {
+export default function Calendar() {
+  const [sheetOpen, setSheetOpen] = useState(false);
+  const [selectedDay, setSelectedDay] = useState<number | null>(null);
   const colorScheme = useColorScheme();
-
 
   function getDaysInMonth(month: number, year: number) {
     return new Date(year, month + 1, 0).getDate();
+  }
+
+  function handleDatePress(day: number) {
+    setSheetOpen(true);
+    setSelectedDay(day);
   }
 
   const renderDaysOfMonth = () => {
@@ -15,9 +23,19 @@ export default function Index() {
     const today = date.getDate();
 
     const days = [];
-    for (let i = 1; i <= getDaysInMonth(date.getMonth(), date.getFullYear()); i++) {
+    for (
+      let i = 1;
+      i <= getDaysInMonth(date.getMonth(), date.getFullYear());
+      i++
+    ) {
       days.push(
-        <CalendarDay key={i} onPress={() => {}} active={i === today}>
+        <CalendarDay
+          key={i}
+          onPress={() => {
+            handleDatePress(i);
+          }}
+          active={i === today}
+        >
           {i}
         </CalendarDay>
       );
@@ -26,17 +44,24 @@ export default function Index() {
   };
 
   return (
-    <PageLayout>
-      <Text
-        className={`font-bold text-5xl ${
-          colorScheme === "dark" ? "text-white" : "text-gray-900"
-        }`}
-      >
-        Calendar
-      </Text>
-      <View className="flex flex-row flex-wrap flex-grow gap-2 mt-4">
-        {renderDaysOfMonth()}
-      </View>
-    </PageLayout>
+    <>
+      <PageLayout>
+        <Text
+          className={`font-bold text-5xl ${
+            colorScheme === "dark" ? "text-white" : "text-gray-900"
+          }`}
+        >
+          Calendar
+        </Text>
+        <View className="flex flex-row flex-wrap flex-grow gap-2 mt-4">
+          {renderDaysOfMonth()}
+        </View>
+      </PageLayout>
+      <CustomBottomSheet isOpen={sheetOpen} onClose={() => setSheetOpen(false)}>
+        <Text className="text-lg font-semibold">The day is {selectedDay}</Text>
+        <Text className="mt-2 text-gray-600">{selectedDay}</Text>
+      </CustomBottomSheet>
+      );
+    </>
   );
 }
